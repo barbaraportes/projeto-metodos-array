2a05df95ba505600875a4332e80f47d23f04b587
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-09-18 18:47:01 -0300

    metodo sort ordena livros por preco

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,10 +3,14 @@ const elementoParaInserirLivros = document.getElementById('livros');
 function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = '';
     listaDeLivros.forEach(livro => {
+
+        //let disponibilidade = verificarDisponibilidadeDoLivro(livro);
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
+
         elementoParaInserirLivros.innerHTML += `
 
             <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+            <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
             <h2 class="livro__titulo">
             ${livro.titulo}
             </h2>
@@ -20,3 +24,15 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         `
     });
 }
+
+
+
+//  ----- CODIGO IGUAL OPERADOR TERNARIO -------
+// function verificarDisponibilidadeDoLivro (livro) {
+//     if (livro.quantidade > 0) {
+//         return 'livro__imagens'
+//     } else {
+//         return 'livro__imagens indisponivel'
+//     }
+// }
+
