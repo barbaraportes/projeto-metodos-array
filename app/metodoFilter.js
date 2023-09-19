a21dfb8ae86e5ec2624ee996df3890b26fbabaff
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-09-18 21:29:45 -0300

    updates

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,5 +1,4 @@
 const botoes = document.querySelectorAll('.btn');
-
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
 
 function filtrarLivros() {
@@ -10,7 +9,8 @@ function filtrarLivros() {
     let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria);
     exibirOsLivrosNaTela(livrosFiltrados);
     if (categoria == 'disponivel') {
-        exibirValorTotalDosLivrosDisponiveisNaTela();
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);      
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal);
     }
 }
 
@@ -22,10 +22,10 @@ function filtrarPorDisponibilidade() {
     return livros.filter(livro => livro.quantidade > 0);
 }
 
-function exibirValorTotalDosLivrosDisponiveisNaTela() {
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
     elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
         <div class="livros__disponiveis">
-        <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+        <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
         </div>    
     `
 }
